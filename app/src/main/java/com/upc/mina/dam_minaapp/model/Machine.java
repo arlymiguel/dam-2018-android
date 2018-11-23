87998f20471581ef15c17a81487597217d96dd26
Miguel Rodriguez <migroalv@gmail.com>
2018-11-22 20:52:04 -0500

    agregue un nuevo atributo

--- app/src/main/java/com/upc/mina/dam_minaapp/model/Machine.java
+++ app/src/main/java/com/upc/mina/dam_minaapp/model/Machine.java
@@ -4,6 +4,7 @@ public class Machine {
 
     private String placa;
     private String piloto;
+    private String nombre;
 
 
     public String getPlaca() {
@@ -20,5 +21,13 @@ public class Machine {
 
     public void setPiloto(String piloto) {
         this.piloto = piloto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
